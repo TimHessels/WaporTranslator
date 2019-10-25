9a61651f59ff8964b8a351edbc816dd9b6b188b6
Tim Hessels <timhessels@hotmail.com>
2019-10-25 16:59:49 +0200

    Add KNMI processing in init

--- LEVEL_1/__init__.py
+++ LEVEL_1/__init__.py
@@ -5,8 +5,8 @@ author: Tim Martijn Hessels
 Created on Sat Sep 28 14:37:58 2019
 """
 
-from WaporTranslator.LEVEL_1 import LEVEL_1_Download_SoilGrids, LEVEL_1_Download_WAPOR, LEVEL_1_Process_MSGCCP, LEVEL_1_Download_ESACCI, LEVEL_1_Download_SRTM, LEVEL_1_Download_GLDAS, LEVEL_1_Download_MODIS, LEVEL_1_AOI, LEVEL_1_Create_Mask
+from WaporTranslator.LEVEL_1 import LEVEL_1_Download_SoilGrids, LEVEL_1_Download_WAPOR, LEVEL_1_Process_MSGCCP, LEVEL_1_Download_ESACCI, LEVEL_1_Download_SRTM, LEVEL_1_Download_GLDAS, LEVEL_1_Download_MODIS, LEVEL_1_AOI, LEVEL_1_Create_Mask, LEVEL_1_Process_KNMI
 
-__all__ = ['LEVEL_1_Download_SoilGrids', 'LEVEL_1_Download_WAPOR', 'LEVEL_1_Process_MSGCCP', 'LEVEL_1_Download_ESACCI', 'LEVEL_1_Download_SRTM', 'LEVEL_1_Download_GLDAS', 'LEVEL_1_Download_MODIS', 'LEVEL_1_AOI', 'LEVEL_1_Create_Mask']
+__all__ = ['LEVEL_1_Download_SoilGrids', 'LEVEL_1_Download_WAPOR', 'LEVEL_1_Process_MSGCCP', 'LEVEL_1_Download_ESACCI', 'LEVEL_1_Download_SRTM', 'LEVEL_1_Download_GLDAS', 'LEVEL_1_Download_MODIS', 'LEVEL_1_AOI', 'LEVEL_1_Create_Mask', 'LEVEL_1_Process_KNMI' ]
 
 __version__ = '0.1'
